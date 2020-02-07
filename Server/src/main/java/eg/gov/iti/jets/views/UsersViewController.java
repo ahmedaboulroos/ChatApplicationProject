05a80d99a90b893ab8fb86a14be1b043e926355c
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-07 23:25:08 +0200

    refactored class name

--- Server/src/main/java/eg/gov/iti/jets/views/UsersViewController.java
+++ Server/src/main/java/eg/gov/iti/jets/views/UsersViewController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SystemUsersViewController implements Initializable {
+public class UsersViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
