e5fe37864c4134c00c81fcaf741c71894d00d0ab
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-07 23:26:59 +0200

    added skeleton for field elements and methods for action handlers

--- Server/src/main/java/eg/gov/iti/jets/views/MainServerViewController.java
+++ Server/src/main/java/eg/gov/iti/jets/views/MainServerViewController.java
@@ -1,31 +1,115 @@
 package eg.gov.iti.jets.views;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainServerViewController implements Initializable {
 
+    @FXML
+    private JFXButton dashboardBtn;
+
+    @FXML
+    private JFXButton usersBtn;
+
+    @FXML
+    private JFXButton announcementsBtn;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Tab welcomeTab;
+
+    @FXML
+    private Tab dashboardTab;
+
+    @FXML
+    private Tab usersTab;
+
+    @FXML
+    private Tab announcementsTab;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private JFXButton serverStatusBtn;
+
+    @FXML
+    private Circle serverStatusCircle;
+
+    private boolean serviceRunning = true;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Parent welcomeScene = FXMLLoader.load(getClass().getResource("/views/WelcomeView.fxml"));
+            welcomeTab.setContent(welcomeScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tabPane.getSelectionModel().select(welcomeTab);
+    }
+
+    @FXML
+    void handleDashboardBtnClick(ActionEvent event) {
+        tabPane.getSelectionModel().select(dashboardTab);
+        title.setText("Dashboard");
 
     }
 
-    public void setMain() {
+    @FXML
+    void handleUsersBtnClick(ActionEvent event) {
+        tabPane.getSelectionModel().select(usersTab);
+        title.setText("System Users");
 
     }
 
-    public void closeWindow() {
-        Platform.exit();
+    @FXML
+    void handleAnnouncementsBtnClick(ActionEvent event) {
+        tabPane.getSelectionModel().select(announcementsTab);
+        title.setText("Announcements");
+
     }
 
-    public void maximizeWindow() {
+    @FXML
+    void handleServiceBtnClick(ActionEvent event) {
 
+        if (serviceRunning) {
+            tabPane.getSelectionModel().select(welcomeTab);
+            title.setText("Chat Application Service");
+            serverStatusBtn.setText("Service Stopped          ");
+            serverStatusCircle.setFill(Color.PINK);
+            dashboardBtn.setDisable(true);
+            usersBtn.setDisable(true);
+            announcementsBtn.setDisable(true);
+            serviceRunning = false;
+        } else {
+            serverStatusBtn.setText("Service Running          ");
+            serverStatusCircle.setFill(Color.LIGHTGREEN);
+            dashboardBtn.setDisable(false);
+            usersBtn.setDisable(false);
+            announcementsBtn.setDisable(false);
+            serviceRunning = true;
+        }
     }
 
-    public void minimizeWindow() {
+    public void setController() {
 
     }
 
