150b2ce3e065219e9555c766568cafe1591961aa
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-14 13:25:14 +0200

    added throws remote exception to interfaces methods

--- Server/src/main/java/eg/gov/iti/jets/models/dao/interfaces/AnnouncementDao.java
+++ Server/src/main/java/eg/gov/iti/jets/models/dao/interfaces/AnnouncementDao.java
@@ -4,21 +4,22 @@ import eg.gov.iti.jets.models.entities.Announcement;
 import eg.gov.iti.jets.models.entities.AnnouncementDelivery;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface AnnouncementDao extends Remote {
     // Create
-    boolean createAnnouncement(Announcement announcement);
+    boolean createAnnouncement(Announcement announcement) throws RemoteException;
 
     // Read
-    Announcement getAnnouncement(int announcementId);
+    Announcement getAnnouncement(int announcementId) throws RemoteException;
 
-    List<AnnouncementDelivery> getAnnouncementDeliveries(int announcementId);
+    List<AnnouncementDelivery> getAnnouncementDeliveries(int announcementId) throws RemoteException;
 
     // Update
-    boolean updateAnnouncement(Announcement announcement);
+    boolean updateAnnouncement(Announcement announcement) throws RemoteException;
 
     // Delete
-    boolean deleteAnnouncement(int announcementId);
+    boolean deleteAnnouncement(int announcementId) throws RemoteException;
 
 }
