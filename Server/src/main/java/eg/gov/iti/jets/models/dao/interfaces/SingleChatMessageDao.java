88e100a5c2bb0fce6888c1e128a696427b2137de
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-14 16:32:06 +0200

    added throws to method signatures

--- Server/src/main/java/eg/gov/iti/jets/models/dao/interfaces/SingleChatMessageDao.java
+++ Server/src/main/java/eg/gov/iti/jets/models/dao/interfaces/SingleChatMessageDao.java
@@ -3,19 +3,20 @@ package eg.gov.iti.jets.models.dao.interfaces;
 
 import eg.gov.iti.jets.models.entities.SingleChatMessage;
 
-import java.sql.SQLException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public interface SingleChatMessageDao {
+public interface SingleChatMessageDao extends Remote {
 
     // Create
-    boolean createSingleChatMessage(SingleChatMessage singleChatMessage) throws SQLException;
+    boolean createSingleChatMessage(SingleChatMessage singleChatMessage) throws RemoteException;
 
     // Read
-    SingleChatMessage getSingleChatMessage(int singleChatMessageId);
+    SingleChatMessage getSingleChatMessage(int singleChatMessageId) throws RemoteException;
 
     // Update
-    boolean updateSingleChatMessage(SingleChatMessage singleChatMessage);
+    boolean updateSingleChatMessage(SingleChatMessage singleChatMessage) throws RemoteException;
 
     // Delete
-    boolean deleteSingleChatMessage(int singleChatMessageId);
+    boolean deleteSingleChatMessage(int singleChatMessageId) throws RemoteException;
 }
