10b391a16e87364c47042b71e7eafddcdcb9695d
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-14 12:26:00 +0200

    changes to announcement ui

--- Server/src/main/java/eg/gov/iti/jets/views/AnnouncementsViewController.java
+++ Server/src/main/java/eg/gov/iti/jets/views/AnnouncementsViewController.java
@@ -1,15 +1,35 @@
 package eg.gov.iti.jets.views;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AnnouncementsViewController implements Initializable {
 
+    @FXML
+    private JFXTextField announcementTf;
+
+    @FXML
+    private JFXButton sendAnnouncementBtn;
+
+    @FXML
+    private TableView<?> announcementsTv;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    void handleSendBtn(ActionEvent event) {
+
+    }
+
 
 }
