df56426bc8109433ceac55c77bb0be5047adce3f
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 11:01:46 +0200

    added login feature

---
+++ Client/src/main/java/eg/gov/iti/jets/views/LoginViewController.java
@@ -0,0 +1,66 @@
+package eg.gov.iti.jets.views;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import eg.gov.iti.jets.models.dao.interfaces.UserDao;
+import eg.gov.iti.jets.models.entities.User;
+import eg.gov.iti.jets.models.network.RMIConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable {
+
+    @FXML
+    private JFXTextField phoneNumberTf;
+
+    @FXML
+    private JFXPasswordField passwordPf;
+
+    @FXML
+    private JFXCheckBox rememberMeCb;
+
+    @FXML
+    private JFXButton signInBtn;
+
+    @FXML
+    private JFXButton signUpBtn;
+
+    @FXML
+    private Label errorLbl;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    void handleSignInBtn(ActionEvent event) {
+        try {
+            UserDao userDao = RMIConnection.getInstance().getUserDao();
+            User user = userDao.getUser(phoneNumberTf.getText(), passwordPf.getText());
+            if (user != null) {
+                System.out.println("hooraaaaa");
+            } else {
+                errorLbl.setText("Invalid PhoneNumber or Password");
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleSignUpBtn(ActionEvent event) {
+
+    }
+
+}
+
