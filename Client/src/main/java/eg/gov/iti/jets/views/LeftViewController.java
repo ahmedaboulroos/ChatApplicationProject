9b267df8988106039a6606f45d50129efdd1fd2c
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 14:57:30 +0200

    updated left view

--- Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
+++ Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
@@ -1,6 +1,23 @@
 package eg.gov.iti.jets.views;
 
+import eg.gov.iti.jets.models.dao.interfaces.UserDao;
+import eg.gov.iti.jets.models.network.RMIConnection;
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.ListView;
+
 public class LeftViewController {
+
+    @FXML
+    private Accordion groupsAccordion;
+
+    @FXML
+    private ListView<?> groupChatsLv;
+
+    @FXML
+    private ListView<?> singleChatsLv;
+
+    private UserDao userDao = RMIConnection.getInstance().getUserDao();
 
     private StageCoordinator stageCoordinator;
 
