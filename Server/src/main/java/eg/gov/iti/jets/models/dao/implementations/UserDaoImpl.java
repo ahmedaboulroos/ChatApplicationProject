5e316631429ddf114724353a370475bc7f103fa3
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 11:02:15 +0200

    updated an existing bug

--- Server/src/main/java/eg/gov/iti/jets/models/dao/implementations/UserDaoImpl.java
+++ Server/src/main/java/eg/gov/iti/jets/models/dao/implementations/UserDaoImpl.java
@@ -126,7 +126,7 @@ public class UserDaoImpl extends UnicastRemoteObject implements UserDao {
         User user = null;
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from APP_USER where PHONE_NUMBER = " + phoneNumber + " and PASSWORD = " + password);
+                    "select * from APP_USER where PHONE_NUMBER = '" + phoneNumber + "' and PASSWORD = '" + password + "'");
             ResultSet resultSet = preparedStatement.executeQuery();
             user = getUserFromResultSet(resultSet);
             preparedStatement.close();
