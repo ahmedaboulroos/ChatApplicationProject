033521089bd35d751aae304db43825e6e511b144
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 10:59:51 +0200

    start rmi connection on startup

--- Client/src/main/java/eg/gov/iti/jets/MainClient.java
+++ Client/src/main/java/eg/gov/iti/jets/MainClient.java
@@ -1,5 +1,6 @@
 package eg.gov.iti.jets;
 
+import eg.gov.iti.jets.models.network.RMIConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,32 +10,30 @@ import javafx.stage.Stage;
 public class MainClient extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Client main...");
         launch(args);
     }
 
     @Override
     public void init() throws Exception {
         System.out.println("Client init...");
+        RMIConnection.getInstance().initConnection();
+        RMIConnection.getInstance().startConnection();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println("Client start...");
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/ContactList.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/RegisterView.fxml"));
         Parent mainServerView = fxmlLoader.load();
         stage.setScene(new Scene(mainServerView));
         stage.setTitle("Chat Application Server - Admin Panel");
         stage.show();
-
-
     }
 
     @Override
     public void stop() throws Exception {
         System.out.println("Client stop...");
-
+        RMIConnection.getInstance().stopConnection();
     }
 
 }
