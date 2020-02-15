b983035a6f0cf4904d87d0cfd2b5fe81fdb52941
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 11:40:20 +0200

    added main chat application ui

---
+++ Client/src/main/java/eg/gov/iti/jets/views/MainChatAppViewController.java
@@ -0,0 +1,11 @@
+package eg.gov.iti.jets.views;
+
+public class MainChatAppViewController {
+
+    private StageCoordinator stageCoordinator;
+
+    public void setStageCoordinator(StageCoordinator stageCoordinator) {
+        this.stageCoordinator = stageCoordinator;
+    }
+
+}
