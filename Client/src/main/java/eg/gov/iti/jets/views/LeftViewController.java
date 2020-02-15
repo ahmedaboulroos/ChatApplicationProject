08aaf1caaf3b8d3cea54eb439c51ef7d09c4be57
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 17:37:09 +0200

    load single, group chats

--- Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
+++ Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
@@ -1,7 +1,10 @@
 package eg.gov.iti.jets.views;
 
 import eg.gov.iti.jets.models.dao.interfaces.UserDao;
+import eg.gov.iti.jets.models.entities.GroupChat;
+import eg.gov.iti.jets.models.entities.SingleChat;
 import eg.gov.iti.jets.models.network.RMIConnection;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
@@ -9,6 +12,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 
 import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LeftViewController implements Initializable {
@@ -21,7 +26,10 @@ public class LeftViewController implements Initializable {
     private Tab groupChatTab;
 
     @FXML
-    private ListView<?> groupChatsLv;
+    private ListView<GroupChat> groupChatsLv;
+
+    @FXML
+    private ListView<SingleChat> singleChatsLv;
 
     @FXML
     private Tab singleChatTab;
@@ -29,20 +37,32 @@ public class LeftViewController implements Initializable {
     @FXML
     private Accordion groupsAccordion;
 
-    @FXML
-    private ListView<?> singleChatsLv;
-
     private UserDao userDao = RMIConnection.getInstance().getUserDao();
-
-    private StageCoordinator stageCoordinator;
-
-    public void setStageCoordinator(StageCoordinator stageCoordinator) {
-        this.stageCoordinator = stageCoordinator;
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadSingleChats();
+        loadGroupChats();
+    }
 
+    private void loadSingleChats() {
+        try {
+            List<SingleChat> singleChats = userDao.getUserSingleChats(StageCoordinator.getInstance().currentUser.getUserId());
+            System.out.println(singleChats);
+            singleChatsLv.setItems(FXCollections.observableList(singleChats));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadGroupChats() {
+        try {
+            List<GroupChat> groupChats = userDao.getUserGroupChats(StageCoordinator.getInstance().currentUser.getUserId());
+            System.out.println(groupChats);
+            groupChatsLv.setItems(FXCollections.observableList(groupChats));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
 }
