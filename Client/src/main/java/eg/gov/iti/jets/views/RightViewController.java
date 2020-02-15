728600da425e2510aa86676c196d530510f1a0e1
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-15 13:47:13 +0200

    added right view

---
+++ Client/src/main/java/eg/gov/iti/jets/views/RightViewController.java
@@ -0,0 +1,11 @@
+package eg.gov.iti.jets.views;
+
+public class RightViewController {
+
+    private StageCoordinator stageCoordinator;
+
+    public void setStageCoordinator(StageCoordinator stageCoordinator) {
+        this.stageCoordinator = stageCoordinator;
+    }
+
+}
