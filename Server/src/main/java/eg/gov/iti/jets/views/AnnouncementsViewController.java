6e1b94a6f57ad78188b844f0b668526d588862ef
nour <nourhan.6050@gmail.com>
2020-02-16 22:26:35 +0200

    implement AnnouncementDeliveryModel

--- Server/src/main/java/eg/gov/iti/jets/views/AnnouncementsViewController.java
+++ Server/src/main/java/eg/gov/iti/jets/views/AnnouncementsViewController.java
@@ -7,7 +7,9 @@ import eg.gov.iti.jets.models.dao.implementations.AnnouncementDeliveryDaoImpl;
 import eg.gov.iti.jets.models.dao.interfaces.AnnouncementDao;
 import eg.gov.iti.jets.models.dao.interfaces.AnnouncementDeliveryDao;
 import eg.gov.iti.jets.models.entities.Announcement;
+import eg.gov.iti.jets.models.entities.AnnouncementDelivery;
 import eg.gov.iti.jets.views.models.AnnouncementViewModel;
+import eg.gov.iti.jets.views.models.AnnouncementDeliveryModel;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -46,7 +48,22 @@ public class AnnouncementsViewController implements Initializable {
     private TableColumn<AnnouncementViewModel, String> announcementTimestamp;
 
     @FXML
-    private TableView<?> announcementsDeliveryTv;
+    private TableView<AnnouncementDeliveryModel> announcementsDeliveryTv;
+    @FXML
+    private TableColumn<AnnouncementDeliveryModel, Integer> announcementDeliveryId;
+
+    @FXML
+    private TableColumn<AnnouncementDeliveryModel, Integer> userId;
+
+    @FXML
+    private TableColumn<AnnouncementDeliveryModel, Integer> announcementDelId;
+
+    @FXML
+    private TableColumn<AnnouncementDeliveryModel, String> announcementDeliveryStatus;
+
+
+    //@FXML
+    //private TableView<?> announcementsDeliveryTv;
 
     public List<AnnouncementViewModel> getAllAnnouncements() throws RemoteException {
         List<AnnouncementViewModel> announcementList = new ArrayList<>();
@@ -58,6 +75,18 @@ public class AnnouncementsViewController implements Initializable {
         return announcementList;
     }
 
+    public List<AnnouncementDeliveryModel> getAnnouncementDeliveries(int announcementId) throws RemoteException {
+
+        List<AnnouncementDeliveryModel> announcementDeliveryList = new ArrayList<>();
+        List<AnnouncementDelivery> announcements = announcementDao.getAnnouncementDeliveries(1);
+        for (int i = 0; i < announcements.size(); i++) {
+            announcementDeliveryList.add(new AnnouncementDeliveryModel(announcements.get(i).getAnnouncementDeliveryId(), announcements.get(i).getUserId(), announcements.get(i).getAnnouncementId(), announcements.get(i).getAnnouncementDeliveryStatus().toString()));
+            System.out.println(announcements.get(i).getAnnouncementDeliveryId() + " " + announcements.get(i).getUserId() + " " + announcements.get(i).getAnnouncementId() + "" + announcements.get(i).getAnnouncementDeliveryStatus().toString());
+        }
+        return announcementDeliveryList;
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -67,6 +96,13 @@ public class AnnouncementsViewController implements Initializable {
             announcementTimestamp.setCellValueFactory(new PropertyValueFactory<AnnouncementViewModel, String>("announcementTimestamp"));
             //announcementsTv.getSelectionModel().selectedIndexProperty().addListener(new RowSelectChangeListener());
 
+
+            announcementsDeliveryTv.setItems(FXCollections.observableList(getAnnouncementDeliveries(11)));
+            announcementDeliveryId.setCellValueFactory(new PropertyValueFactory<AnnouncementDeliveryModel, Integer>("announcementDeliveryId"));
+            userId.setCellValueFactory(new PropertyValueFactory<AnnouncementDeliveryModel, Integer>("userId"));
+            announcementDelId.setCellValueFactory(new PropertyValueFactory<AnnouncementDeliveryModel, Integer>("announcementDelId"));
+
+            announcementDeliveryStatus.setCellValueFactory(new PropertyValueFactory<AnnouncementDeliveryModel, String>("announcementDeliveryStatus"));
         } catch (RemoteException e) {
             e.printStackTrace();
         }
@@ -89,6 +125,11 @@ public class AnnouncementsViewController implements Initializable {
     @FXML
     void handleAnnouncementsMc(MouseEvent event) {
 //        announcementsTv.getSelectionModel().getSelectedItem();
+        try {
+            getAnnouncementDeliveries(1);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     void updateAnnouncementsTable() {
