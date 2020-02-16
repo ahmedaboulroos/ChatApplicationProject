4680f318c98e6bf498439be5beb50e6e7d4177e1
martinaihab <martina.ihab96@gmail.com>
2020-02-16 08:51:37 +0200

    Implement LeftViewController: loadContacts and loadGroups

--- Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
+++ Client/src/main/java/eg/gov/iti/jets/views/LeftViewController.java
@@ -1,29 +1,21 @@
 package eg.gov.iti.jets.views;
 
+import eg.gov.iti.jets.models.dao.interfaces.GroupDao;
 import eg.gov.iti.jets.models.dao.interfaces.UserDao;
-import eg.gov.iti.jets.models.entities.GroupChat;
-import eg.gov.iti.jets.models.entities.SingleChat;
-import eg.gov.iti.jets.models.entities.Relationship;
-import eg.gov.iti.jets.models.entities.User;
+import eg.gov.iti.jets.models.entities.*;
 import eg.gov.iti.jets.models.entities.enums.RelationshipStatus;
 import eg.gov.iti.jets.models.network.RMIConnection;
-import javafx.beans.Observable;
-import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.rmi.RemoteException;
-import java.util.List;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class LeftViewController implements Initializable {
 
@@ -47,48 +39,98 @@ public class LeftViewController implements Initializable {
 
     private UserDao userDao = RMIConnection.getInstance().getUserDao();
 
-    private StageCoordinator stageCoordinator;
+    private Map<Integer, ObservableList<Node>> accordionLists = new HashMap<>();
 
-    public void setStageCoordinator(StageCoordinator stageCoordinator) {
-        this.stageCoordinator = stageCoordinator;
-    }
-
-    User user = stageCoordinator.currentUser;
-
-    private HashMap<Integer, ObservableList<Node>> accordionLists;
+    private ClientStageCoordinator clientStageCoordinator;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        clientStageCoordinator = ClientStageCoordinator.getInstance();
+        loadContacts();
+        loadGroups();
+        loadSingleChats();
+        loadGroupChats();
+    }
+
+    private void loadGroups() {
+        User user = clientStageCoordinator.currentUser;
+        int userId = user.getUserId();
+        try {
+            List<Group> userGroups = userDao.getUserGroups(userId);
+            TitledPane allContactsTPane = new TitledPane();
+            allContactsTPane.setText("All Contacts");
+            //accordionLists.put(0, allContactsTPane.getContent());
+            List<Integer> groupIds = new ArrayList<>();
+            for (Group g : userGroups) {
+                TitledPane titledPane = new TitledPane();
+                titledPane.setText(g.getGroupName());
+                List<String> groupUserNames = getGroupUsersNames(g);
+                VBox vBox = new VBox();
+                groupUserNames.forEach(s -> vBox.getChildren().add(new Label(s)));
+                titledPane.setContent(vBox);
+                groupsAccordion.getPanes().add(titledPane);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> getGroupUsersNames(Group g) throws RemoteException {
+        GroupDao groupDao = RMIConnection.getInstance().getGroupDao();
+        //get ids of users in group
+        List<Integer> groupUsersIds = new ArrayList<>();
+        List<GroupContact> groupContacts = groupDao.getGroupContacts(g.getGroupId());
+        groupContacts.stream()
+                .map(GroupContact::getUserId)
+                .forEach(groupUsersIds::add);
+
+        List<String> groupUserNames = new ArrayList<>();
+        if (groupUsersIds.isEmpty())
+            groupUsersIds = null;
+        else {
+            for (Integer id : groupUsersIds) {
+                User user = null;
+                user = userDao.getUser(id);
+                groupUserNames.add(user.getUsername() == null ?
+                        user.getPhoneNumber() : user.getUsername());
+            }
+        }
+
+        return groupUserNames;
+    }
+
+    private void loadContacts() {
+        User user = clientStageCoordinator.currentUser;
         int userId = user.getUserId();
         try {
             List<Relationship> userRelationships = userDao.getUserRelationships(userId);
             TitledPane allContactsTPane = new TitledPane();
             allContactsTPane.setText("All Contacts");
-            accordionLists.put(0, allContactsTPane.getChildrenUnmodifiable());
-            groupsAccordion.getChildrenUnmodifiable().add(allContactsTPane);
+            //accordionLists.put(0, allContactsTPane.getContent());
             List<Integer> friendIds = new ArrayList<>();
-            for(Relationship r: userRelationships) {
-                if(r.getRelationshipStatus()== RelationshipStatus.ACCEPTED) {
+            for (Relationship r : userRelationships) {
+                if (r.getRelationshipStatus() == RelationshipStatus.ACCEPTED) {
                     int id = r.getFirstUserId();
-                    if (id == user.getUserId())
+                    if (id != user.getUserId())
                         friendIds.add(id);
                     else
                         friendIds.add(r.getSecondUserId());
                 }
             }
             List<User> friends = new ArrayList<>();
-            for(Integer i: friendIds)
+            for (Integer i : friendIds)
                 friends.add(userDao.getUser(i));
             List<Label> names = new ArrayList<>();
             friends.stream()
-                    .map(f->f.getUsername()==null?f.getUsername():f.getPhoneNumber())
-                    .forEach(s->names.add(new Label(s)));
-            allContactsTPane.getChildrenUnmodifiable().addAll(names);
+                    .map(f -> f.getUsername() == null ? f.getPhoneNumber() : f.getUsername())
+                    .forEach(s -> names.add(new Label(s)));
+            VBox vBox = new VBox();
+            vBox.getChildren().addAll(names);
+            allContactsTPane.setContent(vBox);
+            groupsAccordion.getPanes().add(allContactsTPane);
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-        loadSingleChats();
-        loadGroupChats();
     }
 
     private void loadSingleChats() {
