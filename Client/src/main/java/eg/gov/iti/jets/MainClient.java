f70b70311075108b25125186137ae63ae6e20f5f
SARA <sara.ayman256@gmail.com>
2020-02-21 13:46:58 +0200

    handel remeber me

--- Client/src/main/java/eg/gov/iti/jets/MainClient.java
+++ Client/src/main/java/eg/gov/iti/jets/MainClient.java
@@ -1,9 +1,21 @@
 package eg.gov.iti.jets;
 
 import eg.gov.iti.jets.controllers.ClientStageCoordinator;
+import eg.gov.iti.jets.models.dao.interfaces.UserDao;
+import eg.gov.iti.jets.models.entities.User;
 import eg.gov.iti.jets.models.network.RMIConnection;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 
 public class MainClient extends Application {
 
@@ -19,10 +31,72 @@ public class MainClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println("Client start...");
-        ClientStageCoordinator coordinator = ClientStageCoordinator.getInstance();
-        coordinator.setStage(primaryStage);
-        coordinator.startLoginScene();
+//        ClientStageCoordinator coordinator = ClientStageCoordinator.getInstance();
+//        coordinator.setStage(primaryStage);
+//        coordinator.startLoginScene();
+        //
+        final String xmlFilePath = "loginFile.xml";
+        File file = new File(xmlFilePath);
+        if (file.exists()) {
+
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = null;
+            try {
+                String phone = null;
+                String password = null;
+                documentBuilder = documentFactory.newDocumentBuilder();
+
+
+                Document document = documentBuilder.parse(file);
+
+                NodeList userInfo = document.getElementsByTagName("User");
+
+                for (int itr = 0; itr < userInfo.getLength(); itr++) {
+                    Node node = userInfo.item(itr);
+
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                        Element element = (Element) node;
+                        phone = element.getElementsByTagName("phone").item(0).getTextContent();
+
+                        password = element.getElementsByTagName("password").item(0).getTextContent();
+                    }
+
+
+                }
+
+                UserDao userDao = RMIConnection.getUserDao();
+                User user = userDao.getUser(phone, password);
+                System.out.println(user.getPhoneNumber());
+                if (user != null) {
+
+                    ClientStageCoordinator coordinator = ClientStageCoordinator.getInstance();
+                    coordinator.currentUser = user;
+                    coordinator.setStage(primaryStage);
+                    coordinator.startMainChatAppScene();
+
+                } else {
+
+                    ClientStageCoordinator coordinator = ClientStageCoordinator.getInstance();
+                    coordinator.setStage(primaryStage);
+                    coordinator.startLoginScene();
+                }
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+
+                e.printStackTrace();
+            }
+
+        } else {
+
+            ClientStageCoordinator coordinator = ClientStageCoordinator.getInstance();
+            coordinator.setStage(primaryStage);
+            coordinator.startLoginScene();
+
+        }
     }
+
 
     @Override
     public void stop() throws Exception {
--- Client/src/main/java/eg/gov/iti/jets/controllers/LoginViewController.java
+++ Client/src/main/java/eg/gov/iti/jets/controllers/LoginViewController.java
@@ -11,10 +11,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginViewController implements Initializable {
 
@@ -36,19 +50,23 @@ public class LoginViewController implements Initializable {
     @FXML
     private Label errorLbl;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
     @FXML
-    void handleSignInBtn(ActionEvent event) {
+    public void handleSignInBtn(ActionEvent event) {
         try {
             UserDao userDao = RMIConnection.getUserDao();
             User user = userDao.getUser(phoneNumberTf.getText(), passwordPf.getText());
             if (user != null) {
                 ClientStageCoordinator.getInstance().currentUser = user;
                 ClientStageCoordinator.getInstance().startMainChatAppScene();
+                if (rememberMeCb.isSelected()) {
+                    rememberMeCbHandelAction();
+                }
             } else {
                 errorLbl.setText("Invalid PhoneNumber or Password");
             }
@@ -66,5 +84,43 @@ public class LoginViewController implements Initializable {
         }
     }
 
+    private void rememberMeCbHandelAction() {
+
+        final String xmlFilePath = "loginFile.xml";
+        try {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+
+            Element elementRoot = document.createElement("User");
+            document.appendChild(elementRoot);
+            Element phoneElement = document.createElement("phone");
+
+            phoneElement.appendChild(document.createTextNode(phoneNumberTf.getText()));
+            elementRoot.appendChild(phoneElement);
+
+            Element name = document.createElement("password");
+            name.appendChild(document.createTextNode(passwordPf.getText()));
+            elementRoot.appendChild(name);
+
+
+            TransformerFactory transformFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+            transformer.transform(source, streamResult);
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+
+        } catch (TransformerException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+    }
+
 }
+
 
