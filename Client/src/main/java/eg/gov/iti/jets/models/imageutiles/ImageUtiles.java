8e8b61126d62fb7ef2946803977735ded6832982
AhmedElnaggar <ahmedalnaggar1995@gmail.com>
2020-02-20 23:52:32 -0800

    correct the Nameing of the ImageUtiles methods and adding convertImageToBytes method

--- Client/src/main/java/eg/gov/iti/jets/models/imageutiles/ImageUtiles.java
+++ Client/src/main/java/eg/gov/iti/jets/models/imageutiles/ImageUtiles.java
@@ -1,15 +1,28 @@
 package eg.gov.iti.jets.models.imageutiles;
 
 import javafx.scene.image.Image;
+import org.apache.commons.io.FileUtils;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Base64;
 
 public class ImageUtiles {
 
-    public static Image FromBytesToImage(byte[] encodedString) {
+    public static Image convertBytesToImage(byte[] encodedString) {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
         ByteArrayInputStream bis = new ByteArrayInputStream(decodedBytes);
         return new Image(bis);
+    }
+
+    public static byte[] convertImageToBytes(String imagePath) {
+        byte[] fileContent = new byte[0];
+        try {
+            fileContent = FileUtils.readFileToByteArray(new File(imagePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 }
