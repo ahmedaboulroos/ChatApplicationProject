c5d513328cfaaf67e53c63f72a2e2c709e2c1e45
alaaalbadry <albadryalaaa@gmail.com>
2020-02-25 08:42:17 +0200

    added setCellfactory for users in AddSingleChatViewController(client)

--- Client/src/main/java/eg/gov/iti/jets/controllers/AddSingleChatViewController.java
+++ Client/src/main/java/eg/gov/iti/jets/controllers/AddSingleChatViewController.java
@@ -13,6 +13,19 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -21,6 +34,17 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class AddSingleChatViewController implements Initializable {
+
+    private static AddSingleChatViewController addSingleChatViewController;
+
+    public static AddSingleChatViewController getInstance() {
+        return addSingleChatViewController;
+    }
+
+    public void setController(AddSingleChatViewController addSingleChatViewController) {
+        this.addSingleChatViewController = addSingleChatViewController;
+    }
+
     SingleChatDao singleChatDao = RMIConnection.getSingleChatDao();
     private User currentUser = ClientStageCoordinator.getInstance().currentUser;
     private int userTwoId;
@@ -57,6 +81,56 @@ public class AddSingleChatViewController implements Initializable {
         List<eg.gov.iti.jets.models.entities.User> contacts = loadContacts();
         ObservableList<eg.gov.iti.jets.models.entities.User> options = FXCollections.observableList(contacts);
         usersCompoBox.setItems(options);
+        usersCompoBox.setCellFactory(usersCompoBox -> new ListCell<eg.gov.iti.jets.models.entities.User>() {
+            // super.updateItem(item, empty);
+            @Override
+            public void updateItem(eg.gov.iti.jets.models.entities.User item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item != null) {
+                    User user = usersCompoBox.getItems().get(0);
+                    HBox hBox = new HBox();
+                    hBox.setStyle("-fx-background-color: transparent  ;" +
+                            "-fx-padding: 1;" + "-fx-border-style: solid inside;"
+                            + "-fx-border-width: 3;" + "-fx-border-insets: 1;"
+                            + "-fx-border-radius: 2;" + "-fx-border-color: white;");
+                    Circle imageCircle = new Circle();
+                    Image imageForTasting = new Image("images/chat-circle-blue-512.png");
+                    imageCircle.setFill(new ImagePattern(imageForTasting));
+                    imageCircle.setRadius(20);
+                    imageCircle.setStroke(Color.NAVY);
+                    imageCircle.setStrokeWidth(1);
+                    StackPane stackPane = new StackPane();
+                    //Region region= new Region();
+                    Region selectedBar = new Region();
+                    // selectedBar.getStyleClass().add(HIGHLIGHT_STYLE_CLASS);
+                    selectedBar.setMinWidth(Region.USE_PREF_SIZE);
+                    selectedBar.setMaxHeight(Region.USE_PREF_SIZE);
+                    selectedBar.setMaxWidth(Double.MAX_VALUE);
+                    StackPane.setAlignment(selectedBar, Pos.BOTTOM_CENTER);
+                    stackPane.getChildren().addAll(imageCircle, selectedBar);
+                    String userInfo;
+                    if (user.getUsername() == null) {
+                        userInfo = user.getPhoneNumber();
+                    } else {
+                        userInfo = user.getUsername();
+                    }
+                    Text text = new Text(userInfo);
+                    text.setFont(Font.font("Arial Rounded MT Bold", FontWeight.BOLD, 20));//FontWeight.BOLD
+                    text.setFill(Color.NAVY);
+                    Label label = new Label();
+                    label.setMinWidth(20);
+                    hBox.getChildren().addAll(stackPane, label, text);
+                    hBox.setAlignment(Pos.CENTER_LEFT);
+                    setPrefWidth(200);
+                    setPrefHeight(60);
+                    hBox.setMaxWidth(200);
+                    hBox.setMinWidth(200);
+                    setGraphic(hBox);
+                }
+            }
+        });
+
     }
 
     private List<User> loadContacts() {
