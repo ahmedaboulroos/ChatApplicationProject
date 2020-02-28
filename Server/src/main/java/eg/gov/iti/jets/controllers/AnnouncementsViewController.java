e11ffa874a957b00858dadeb9a36ca782f02bfbc
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-28 16:23:45 +0200

    updated announcements table view selection

--- Server/src/main/java/eg/gov/iti/jets/controllers/AnnouncementsViewController.java
+++ Server/src/main/java/eg/gov/iti/jets/controllers/AnnouncementsViewController.java
@@ -4,7 +4,6 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import eg.gov.iti.jets.models.dao.implementations.AnnouncementDaoImpl;
 import eg.gov.iti.jets.models.dao.interfaces.AnnouncementDao;
-import eg.gov.iti.jets.models.dto.AnnouncementsTableModel;
 import eg.gov.iti.jets.models.entities.Announcement;
 import eg.gov.iti.jets.models.persistence.DBConnection;
 import javafx.collections.FXCollections;
@@ -15,15 +14,12 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebView;
 
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class AnnouncementsViewController implements Initializable {
@@ -39,32 +35,34 @@ public class AnnouncementsViewController implements Initializable {
     @FXML
     private JFXButton sendAnnouncementBtn;
     @FXML
-    private TableView<AnnouncementsTableModel> announcementsTv;
+    private TableView<Announcement> announcementsTv;
     @FXML
-    private TableColumn<AnnouncementsTableModel, Integer> announcementId;
+    private TableColumn<Announcement, Integer> announcementId;
     @FXML
-    private TableColumn<AnnouncementsTableModel, String> announcementTitle;
+    private TableColumn<Announcement, String> announcementTitle;
     @FXML
-    private TableColumn<AnnouncementsTableModel, String> announcementTimestamp;
-
-    public List<AnnouncementsTableModel> getAllAnnouncements() throws RemoteException {
-        List<AnnouncementsTableModel> announcementList = new ArrayList<>();
-        List<Announcement> announcements = announcementDao.getAllAnnouncements();
-        for (int i = 0; i < announcements.size(); i++) {
-            announcementList.add(new AnnouncementsTableModel(announcements.get(i).getId(), announcements.get(i).getTitle(), announcements.get(i).getSendDateTime().toLocalDate().toString()));
-        }
-        return announcementList;
-    }
+    private TableColumn<Announcement, String> announcementTimestamp;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             announcementsTv.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-            announcementsTv.setItems(FXCollections.observableList(getAllAnnouncements()));
+            announcementsTv.setItems(FXCollections.observableList(announcementDao.getAllAnnouncements()));
 
-            announcementId.setCellValueFactory(new PropertyValueFactory<AnnouncementsTableModel, Integer>("announcementId"));
-            announcementTitle.setCellValueFactory(new PropertyValueFactory<AnnouncementsTableModel, String>("announcementTitle"));
-            announcementTimestamp.setCellValueFactory(new PropertyValueFactory<AnnouncementsTableModel, String>("announcementTimestamp"));
+            announcementId.setCellValueFactory(new PropertyValueFactory<>("id"));
+            announcementTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+            announcementTimestamp.setCellValueFactory(new PropertyValueFactory<>("sendDateTime"));
+
+            announcementsTv.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+                if (newSelection != null) {
+                    try {
+                        contentWv.getEngine().loadContent(announcementDao.getAnnouncement(newSelection.getId()).getContent());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
         } catch (RemoteException e) {
             e.printStackTrace();
         }
@@ -87,21 +85,9 @@ public class AnnouncementsViewController implements Initializable {
     void updateAnnouncementsTable() {
         try {
             System.out.println(announcementDao.getAllAnnouncements());
-            announcementsTv.setItems(FXCollections.observableList(getAllAnnouncements()));
+            announcementsTv.setItems(FXCollections.observableList(announcementDao.getAllAnnouncements()));
         } catch (RemoteException e) {
             e.printStackTrace();
-        }
-    }
-
-    @FXML
-    void handleAnnouncementSelection(MouseEvent event) {
-        AnnouncementsTableModel selectedItem = announcementsTv.getSelectionModel().getSelectedItem();
-        if (selectedItem != null) {
-            try {
-                contentWv.getEngine().loadContent(announcementDao.getAnnouncement(selectedItem.getAnnouncementId()).getContent());
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
         }
     }
 
