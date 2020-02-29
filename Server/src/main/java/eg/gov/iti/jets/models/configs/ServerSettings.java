dd78367ab4b02b8eb2719fda6b383f238d614e14
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-29 15:27:42 +0200

    updated server settings properties

--- Server/src/main/java/eg/gov/iti/jets/models/configs/ServerSettings.java
+++ Server/src/main/java/eg/gov/iti/jets/models/configs/ServerSettings.java
@@ -9,38 +9,15 @@ import javax.xml.bind.annotation.XmlType;
 public class ServerSettings {
     private String serverUsername;
     private String serverPassword;
-    private String databaseUrl;
-    private int databasePort;
-    private String databaseUsername;
-    private String databasePassword;
+
 
     public ServerSettings() {
 
     }
 
-    public ServerSettings(String serverUsername, String serverPassword, String databaseUrl, int databasePort, String databaseUsername, String databasePassword) {
+    public ServerSettings(String serverUsername, String serverPassword) {
         this.serverUsername = serverUsername;
         this.serverPassword = serverPassword;
-        this.databaseUrl = databaseUrl;
-        this.databasePort = databasePort;
-        this.databaseUsername = databaseUsername;
-        this.databasePassword = databasePassword;
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-    @Override
-    public String toString() {
-        return "ServerSettings{" +
-                "serverUsername='" + serverUsername + '\'' +
-                ", serverPassword='" + serverPassword + '\'' +
-                ", databaseUrl='" + databaseUrl + '\'' +
-                ", databasePort=" + databasePort +
-                ", databaseUsername='" + databaseUsername + '\'' +
-                ", databasePassword='" + databasePassword + '\'' +
-                '}';
     }
 
     public String getServerUsername() {
@@ -61,39 +38,11 @@ public class ServerSettings {
         this.serverPassword = serverPassword;
     }
 
-    public String getDatabaseUrl() {
-        return databaseUrl;
-    }
-
-    @XmlElement(name = "database_url")
-    public void setDatabaseUrl(String databaseUrl) {
-        this.databaseUrl = databaseUrl;
-    }
-
-    public int getDatabasePort() {
-        return databasePort;
-    }
-
-    @XmlElement(name = "database_port")
-    public void setDatabasePort(int databasePort) {
-        this.databasePort = databasePort;
-    }
-
-    public String getDatabaseUsername() {
-        return databaseUsername;
-    }
-
-    @XmlElement(name = "database_username")
-    public void setDatabaseUsername(String databaseUsername) {
-        this.databaseUsername = databaseUsername;
-    }
-
-    public String getDatabasePassword() {
-        return databasePassword;
-    }
-
-    @XmlElement(name = "database_password")
-    public void setDatabasePassword(String databasePassword) {
-        this.databasePassword = databasePassword;
+    @Override
+    public String toString() {
+        return "ServerSettings{" +
+                "serverUsername='" + serverUsername + '\'' +
+                ", serverPassword='" + serverPassword + '\'' +
+                '}';
     }
 }
