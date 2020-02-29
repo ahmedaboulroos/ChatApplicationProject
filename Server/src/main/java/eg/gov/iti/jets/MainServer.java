bdb100c06cb4541c0dbc18b55da4b87f0e6403fd
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-02-29 15:27:10 +0200

    moved database configurations into external xml file

--- Server/src/main/java/eg/gov/iti/jets/MainServer.java
+++ Server/src/main/java/eg/gov/iti/jets/MainServer.java
@@ -1,11 +1,13 @@
 package eg.gov.iti.jets;
 
 import eg.gov.iti.jets.controllers.ServerStageCoordinator;
+import eg.gov.iti.jets.models.configs.DatabaseSettings;
 import eg.gov.iti.jets.models.configs.ServerSettings;
 import eg.gov.iti.jets.models.network.RMIConnection;
 import eg.gov.iti.jets.models.persistence.DBConnection;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import oracle.net.ano.AnoServices;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -28,43 +30,8 @@ public class MainServer extends Application {
 
     @Override
     public void init() throws Exception {
-        try {
-            JAXBContext context = JAXBContext.newInstance(ServerSettings.class);
-
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = schemaFactory.newSchema(new File("src/main/resources/configs/server_settings.xsd"));
-            marshaller.setSchema(schema);
-
-            File xmlFileSettings = new File("src/main/resources/configs/server_settings.xsd");
-            if (!xmlFileSettings.exists()) {
-                ServerSettings serverSettings = new ServerSettings();
-                serverSettings.setServerUsername("amin");
-                serverSettings.setServerPassword("amin");
-                serverSettings.setDatabaseUrl("localhost");
-                serverSettings.setDatabasePort(1234);
-                serverSettings.setDatabaseUsername("CHATAPP");
-                serverSettings.setDatabasePassword("CHATAPP");
-                marshaller.marshal(serverSettings, new FileOutputStream("src/main/resources/configs/server_settings.xml"));
-            }
-
-            ServerSettings newServerSettings = (ServerSettings) context.createUnmarshaller().unmarshal(new FileReader("src/main/resources/configs/server_settings.xml"));
-            System.out.println(newServerSettings);
-
-            Properties properties = new Properties();
-            properties.setProperty("serverUsername", newServerSettings.getServerUsername());
-            properties.setProperty("serverPassword", newServerSettings.getServerPassword());
-            properties.setProperty("databaseUrl", newServerSettings.getDatabaseUrl());
-            properties.setProperty("databasePort", Integer.toString(newServerSettings.getDatabasePort()));
-            properties.setProperty("databaseUsername", newServerSettings.getDatabaseUsername());
-            properties.setProperty("databasePassword", newServerSettings.getDatabasePassword());
-
-            ServerStageCoordinator.setServerProperties(properties);
-        } catch (JAXBException | SAXException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        loadServerSettings();
+        loadDatabaseSettings();
     }
 
     @Override
@@ -80,4 +47,73 @@ public class MainServer extends Application {
         System.exit(0);
     }
 
+    private void loadDatabaseSettings() {
+        try {
+            JAXBContext context = JAXBContext.newInstance(DatabaseSettings.class);
+
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("src/main/resources/configs/database_settings.xsd"));
+            marshaller.setSchema(schema);
+
+            File xmlFileSettings = new File("src/main/resources/configs/database_settings.xsd");
+            if (!xmlFileSettings.exists()) {
+                DatabaseSettings databaseSettings = new DatabaseSettings();
+                databaseSettings.setDatabaseUrl("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))" + "(CONNECT_DATA=(SERVICE_NAME=XE)))");
+                databaseSettings.setDatabaseUsername("CHATAPP");
+                databaseSettings.setDatabasePassword("CHATAPP");
+                databaseSettings.setEncryptionType("( " + AnoServices.ENCRYPTION_AES256 + "," + AnoServices.ENCRYPTION_AES192 + ")");
+                databaseSettings.setChecksumType("( " + AnoServices.CHECKSUM_SHA1 + " )");
+                marshaller.marshal(databaseSettings, new FileOutputStream("src/main/resources/configs/database_settings.xml"));
+            }
+
+            DatabaseSettings newDatabaseSettings = (DatabaseSettings) context.createUnmarshaller().unmarshal(new FileReader("src/main/resources/configs/database_settings.xml"));
+            System.out.println(newDatabaseSettings);
+
+            Properties databaseProperties = new Properties();
+            databaseProperties.setProperty("databaseUrl", newDatabaseSettings.getDatabaseUrl());
+            databaseProperties.setProperty("databaseUsername", newDatabaseSettings.getDatabaseUsername());
+            databaseProperties.setProperty("databasePassword", newDatabaseSettings.getDatabasePassword());
+            databaseProperties.setProperty("encryptionType", newDatabaseSettings.getEncryptionType());
+            databaseProperties.setProperty("checksumType", newDatabaseSettings.getChecksumType());
+
+            DBConnection.setDatabaseProperties(databaseProperties);
+        } catch (JAXBException | FileNotFoundException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadServerSettings() {
+        try {
+            JAXBContext context = JAXBContext.newInstance(ServerSettings.class);
+
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File("src/main/resources/configs/server_settings.xsd"));
+            marshaller.setSchema(schema);
+
+            File xmlFileSettings = new File("src/main/resources/configs/server_settings.xsd");
+            if (!xmlFileSettings.exists()) {
+                ServerSettings serverSettings = new ServerSettings();
+                serverSettings.setServerUsername("amin");
+                serverSettings.setServerPassword("amin");
+                marshaller.marshal(serverSettings, new FileOutputStream("src/main/resources/configs/server_settings.xml"));
+            }
+
+            ServerSettings newServerSettings = (ServerSettings) context.createUnmarshaller().unmarshal(new FileReader("src/main/resources/configs/server_settings.xml"));
+            System.out.println(newServerSettings);
+
+            Properties properties = new Properties();
+            properties.setProperty("serverUsername", newServerSettings.getServerUsername());
+            properties.setProperty("serverPassword", newServerSettings.getServerPassword());
+
+            ServerStageCoordinator.setServerProperties(properties);
+        } catch (JAXBException | FileNotFoundException | SAXException e) {
+            e.printStackTrace();
+        }
+    }
 }
